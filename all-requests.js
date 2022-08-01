fb29c8e2415baeae92926f908c34a8dd28fcc560
RezaSoltanmoradi <rezasoltanmoradi1995@gmail.com>
2022-07-31 23:39:08 -0700

    fix axios request

--- all-requests.js
+++ all-requests.js
@@ -1,25 +1,28 @@
 import axios from "axios";
-import { Axios } from "./helper/axios";
+// import { Axios } from "./helper/axios";
+
+const baseUrl = "https://fakestoreapi.com";
 
 export const getAllProducts = async () => {
-    const request = await Axios.get("/products");
+    const request = await axios.get(`${baseUrl}/products`);
     return request.data;
 };
 
 export const getProductById = async (productId) => {
-    const request = await Axios.get(`/products/${productId}`);
+    const request = await axios.get(`${baseUrl}/products/${productId}`);
     return request.data;
 };
 
 export const getAllCategories = async () => {
-    const request = await Axios.get("/products/categories");
+    const request = await axios.get(`${baseUrl}/products/categories`);
 
     return request.data;
 };
 
 export const getCategoryById = async (categoryId) => {
     const convertId = categoryId?.replace(/-/g, " ");
-    const request = await Axios.get(`/products/category/${convertId}`);
+    const request = await axios.get(
+        `${baseUrl}/products/category/${convertId}`
+    );
     return request.data;
 };
-    
